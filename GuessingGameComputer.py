301e20b7ff2fd798db9e36363eb63ea35270aaa4
AlliysonBonner <alliyson.bonner@gmail.com>
2023-02-16 23:42:18 -0600

    Update Guessing Game project with image, text, and license  Added image and updated README.md with more details on the project. Added LICENSE.md to clarify the terms of use.

--- GuessingGameComputer.py
+++ GuessingGameComputer.py
@@ -1,62 +1,44 @@
-# Guessing Game - Computer agent
-# The user comes up with a secret number and we can have the computer try to guess it.
-# The computer takes a guess and the user gives the computer feedback. the computer 
-# takes this feed back an incorperates this knowledge to help it make its next guess.
-
-# Author: Alliyson Bonner
-# Github: https://github.com/alliysonbonner
-# Linkedin: https://www.linkedin.com/in/alliyson-bonner-6404976b/
-# Email: alliyson.bonner@gmail.com
-# Date: December 07 2022
-
-# Import libraries
 import random
 
-def computer_guess(highest_number):
-  """Starts the guess game where the computer must guess the correct
-  number to win.
-  Args:
-      highest_number(int): This is the highest posible number that
-      can be guessed.
-  """
-  # Set lowest number the computer can guess to 1. 
-  # Set the highest number the computer can guess to highest_number.
-  low = 1
-  high = highest_number
-  
-  # Set feedback to empty string. Get guesses from the computer until
-  # the guess is correct.
-  feedback = ''
-  while feedback != 'c':
-  	# Set guess to a random number between low and high, inclusivly, 
-    # if low does not equal high. Set guess to low or high if low 
-    # equals high.
-    if low != high:
-      guess = random.randint(low, high) 
-    else:
-      guess = low # could also be high b/c low = high
-  	
-    # Prompt user to respond to the guess given by the computer with 
-    # H, L, or C depending on if the computer's guess is too high,
-    # too low, or correct, respectiively. If the computer guesses 
-    # incorrectly, modify the computer's guessing range for the next
-    # guess.
-    feedback = input(f'Is {guess} too high (H), too low (L) or correct (C)?: ').lower()
-    if feedback == 'h':
-      high = guess - 1
-    elif feedback == 'l':
-      low = guess + 1
-    
-  # Give response when the computer guesees correctly.
-  print(f'Whoop! Whoop! The computer guessed your number, {guess}, corrrectly!!')
-    
+def computer_guess(highest_number: int) -> None:
+    """Starts the guessing game where the computer must guess the user's secret number.
+
+    Args:
+        highest_number (int): The highest possible number that can be guessed.
+    """
+    # Set the initial guessing range for the computer.
+    low = 1
+    high = highest_number
+    feedback = ''  # Initialize feedback to an empty string.
+
+    # Keep guessing until the computer gets the correct number.
+    while feedback != 'c':
+        if low != high:
+            # If the current range has more than one number, choose a random number.
+            guess = random.randint(low, high)
+        else:
+            # If the current range has only one number, choose that number.
+            guess = low
+
+        # Get user feedback on the computer's guess.
+        feedback = input(f'Is {guess} too high (H), too low (L) or correct (C)?: ').lower()
+
+        # Modify the computer's guessing range based on the user feedback.
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    # Print a message when the computer guesses the correct number.
+    print(f'The computer guessed your number, {guess}, correctly!')
+
 if __name__ == "__main__":
-  # Display welcome message
-  print("Welcome to the guessing game")
-  # Keep prompting user to choose a number until their number is higher than 1
-  highest_number = 0
-  while highest_number <= 1:
-  	highest_number = int(input("What's the highest possible secret number in this \
-game: "))
-  # Start guessing game
-  computer_guess(highest_number)
+    print("Welcome to the guessing game")
+
+    # Prompt user to choose the highest possible secret number.
+    highest_number = 0
+    while highest_number <= 1:
+        highest_number = int(input("What's the highest possible secret number in this game: "))
+
+    # Start the guessing game.
+    computer_guess(highest_number)
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# GuessingGameComputer
+# Guessing Game - Computer Agent
+![Illustration of a computer trying to guess a secret number, with a thought bubble containing a question mark or a number.](https://github.com/AlliysonBonner/GuessingGameComputer/blob/main/image.png?raw=true)
+This is a simple guessing game where the computer tries to guess the user's secret number. The user is prompted to enter the highest possible number that the computer can guess, and the computer will make random guesses and adjust its guessing range based on the user's feedback until it correctly guesses the user's number.
+
+## Getting Started
+To get started with this project, you can clone the repository and run the code locally on your computer. The code is written in Python, so you will need to have Python installed to run it.
+
+## Usage
+To run the guessing game, simply run the computer_guess function in the guessing_game.py file. The function takes one argument, which is the highest possible number that the computer can guess.
+
+## Contributing
+If you would like to contribute to this project, you can fork the repository and submit a pull request with your changes. Be sure to include a detailed description of the changes you have made and why you think they are necessary.
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/alliysonbonner/GuessingGameComputer/LICENSE.md) file for details.
+
+## Acknowledgments
+This project was inspired by the "Guess the Number" game from the book "Invent Your Own Computer Games with Python" by Al Sweigart.
+
+## Contact
+For questions or comments about the Guessing Game, please contact the author:
+
+- Name: Alliyson Bonner
+- LinkedIn: https://www.linkedin.com/in/alliyson-bonner-6404976b/
+- GitHub: https://github.com/alliysonbonner
